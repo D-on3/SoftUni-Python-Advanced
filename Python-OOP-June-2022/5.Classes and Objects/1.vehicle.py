c81824f6ed1cf0e6da956c061b8bb58c1f727df6
Deyan Nedyalkov <104354779+d-on3@users.noreply.github.com>
2022-07-25 19:44:33 +0300

    init
    Signed-off-by: D-on3 <dnedyalakov396@gmail.com>

--- Python-OOP-June-2022/5.Classes and Objects/1.vehicle.py
+++ Python-OOP-June-2022/5.Classes and Objects/1.vehicle.py
@@ -1,3 +1,8 @@
+# Create a class called Vehicle. Upon initialization, it should receive max_speed
+# (integer, optional; 150 by default) and mileage (number).
+# Create an instance variable called gadgets - an empty list by default.
+
+
 class Vehicle:
     def __init__(self, mileage, max_speed=150):
         self.max_speed = max_speed
