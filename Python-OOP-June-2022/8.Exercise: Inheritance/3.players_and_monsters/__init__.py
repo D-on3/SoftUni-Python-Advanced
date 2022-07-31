b02bd15e82c56459853024d49e8c2a18f5308a06
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-07-31 21:39:10 +0300

    init
    Signed-off-by: D-on3 <104354779+D-on3@users.noreply.github.com>

---
+++ Python-OOP-June-2022/8.Exercise: Inheritance/3.players_and_monsters/__init__.py
@@ -0,0 +1,19 @@
+from project.hero import Hero
+from project.elf import Elf
+# from project.muself import MuseElf
+# from project.wizard import Wizard
+# from project.darkwizard import DarkWizard
+# from project.soulmaster import Soulmaster
+# from project.knight import Knight
+# from project.blade import Blade
+
+
+hero = Hero("H", 4)
+print(hero.username)
+print(hero.level)
+print(str(hero))
+elf = Elf("E", 4)
+print(str(elf))
+print(elf.__class__.__bases__[0].__name__)
+print(elf.username)
+print(elf.level)
