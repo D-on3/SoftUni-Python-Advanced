3825b48ec1a6f76ce3f346e5e03e780f828c8ea4
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-07-31 21:14:50 +0300

    init
    Signed-off-by: D-on3 <104354779+D-on3@users.noreply.github.com>

---
+++ Python-OOP-June-2022/8.Exercise: Inheritance/1.food/1.person.py
@@ -0,0 +1,21 @@
+class Person():
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+
+
+class Child(Person):
+    def __init__(self,name,age):
+        super().__init__(name,age)
+
+
+
+
+
+person = Person("Peter", 25)
+child = Child("Peter Junior", 5)
+print(person.name)
+print(person.age)
+print(child.__class__.__bases__[0].__name__)
