58f9dc9fd9346bb3929be4c26700059131c83274
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-08-08 00:35:53 +0300

    init
    Signed-off-by: D-on3 <104354779+D-on3@users.noreply.github.com>

---
+++ Python-OOP-June-2022/7.Inheritance/4.multilevel_inheritence /project/sports_car.py
@@ -0,0 +1,15 @@
+from project2 import Car
+
+
+class SportCar(Car):
+    def race(self):
+        return 'racing...'
+
+
+
+
+
+# car = SportCar()
+# print(car.move())
+# print(car.race())
+# print(car.drive())
