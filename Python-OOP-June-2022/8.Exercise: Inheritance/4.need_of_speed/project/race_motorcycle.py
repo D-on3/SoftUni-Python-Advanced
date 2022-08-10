c8349a7256c51e47a0e840c94cfbaaea9ee45a8c
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-08-10 16:32:30 +0300

    init
    Signed-off-by: D-on3 <104354779+D-on3@users.noreply.github.com>

---
+++ Python-OOP-June-2022/8.Exercise: Inheritance/4.need_of_speed/project/race_motorcycle.py
@@ -0,0 +1,6 @@
+from project.motorcycle import Motorcycle
+
+class RaceMotorcycle(Motorcycle):
+    DEFAULT_FUEL_CONSUMPTION = 8
+    def __init__(self, fuel: float, horse_power: int):
+        super().__init__(fuel, horse_power)
