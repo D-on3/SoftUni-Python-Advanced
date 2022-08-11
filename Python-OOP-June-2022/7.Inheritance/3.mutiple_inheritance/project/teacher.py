be779cf7bde3ddcf3d11dd6a3d043d6a54cbead6
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-08-11 16:08:23 +0300

    init
    Signed-off-by: D-on3 <104354779+D-on3@users.noreply.github.com>

---
+++ Python-OOP-June-2022/7.Inheritance/3.mutiple_inheritance/project/teacher.py
@@ -0,0 +1,18 @@
+from project2 import Employee
+from project2 import Person
+
+
+
+class Teacher(Employee,Person):
+
+    def teach(self):
+        return "teach..."
+
+
+
+
+
+# a= Teacher()
+# print(a.teach())
+# print(a.get_fired())
+# print(a.sleep())
