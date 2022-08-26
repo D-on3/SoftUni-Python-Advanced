f99f451b8b02cc31ff21227633f9b7d1b980a4b2
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-08-26 19:44:06 +0300

    init
    Signed-off-by: D-on3 <104354779+D-on3@users.noreply.github.com>

--- Python-OOP-June-2022/10.Exercise: Encapsulation/1.wild_cat_zoo/project/cheetah.py
+++
@@ -1,9 +0,0 @@
-from project.animal import Animal
-
-
-class Cheetah(Animal):
-    MONEY_FOR_CARE = 60
-
-    def __init__(self, name, gender, age):
-        super().__init__(name, gender, age)
-        self.money_for_care = Cheetah.MONEY_FOR_CARE
