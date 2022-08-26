180d10e40b658d8dd03c6ea3ac2212a9be673f65
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-08-26 19:44:39 +0300

    init
    Signed-off-by: D-on3 <104354779+D-on3@users.noreply.github.com>

--- Python-OOP-June-2022/8.Exercise: Inheritance/3.players_and_monsters/project/dark_wizard.py
+++
@@ -1,6 +0,0 @@
-from project.wizard import Wizard
-
-
-class DarkWizard(Wizard):
-    def __init__(self, username: str, level: int):
-        super().__init__(username, level)
--- Python-OOP-June-2022/8.Exercise: Inheritance/5.shop/project/drink.py
+++
@@ -1,8 +0,0 @@
-from project.product import Product
-
-
-class Drink(Product):
-    QUANTITY = 10
-    def __init__(self,name):
-        super().__init__(name,self.QUANTITY)
-
