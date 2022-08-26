05206385865c44e41b97e3e6279cd8595c6f6ff7
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-08-26 19:44:12 +0300

    init
    Signed-off-by: D-on3 <104354779+D-on3@users.noreply.github.com>

--- Python-OOP-June-2022/8.Exercise: Inheritance/4.need_of_speed/project/cross_motorcycle.py
+++
@@ -1,7 +0,0 @@
-from project.motorcycle import Motorcycle
-
-class CrossMotorcycle(Motorcycle):
-
-
-    def __init__(self, fuel: float, horse_power: int):
-        super().__init__(fuel, horse_power)
