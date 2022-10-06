2815fce4b2b4dc30501e5279dbcf5cb3b3098cc3
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-10-06 17:14:56 +0300

    init

---
+++ Python-OOP-June-2022/to check/other/project/calc.py
@@ -0,0 +1,25 @@
+import  tkinter as tk
+
+
+
+class Calculator(tk.Tk):
+
+    def __init__(self):
+        #App window
+        self.window = tk.Tk()
+        # App size
+        self.window.geometry("400x400")
+        self.window.resizable(0,0)
+        self.window.title("Some Calc")
+
+
+
+
+    def run(self):
+        self.window.mainloop()
+
+
+
+if __name__ == '__main__':
+    calc = Calculator()
+    calc.run()
