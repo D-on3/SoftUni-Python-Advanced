ff9d5ce02e7eb20ddd93ba471197f5f793123f24
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-10-08 13:35:43 +0300

    init

--- Python-OOP-June-2022/10.Exercise:Encapsulation/1.wild_cat_zoo/project/animal.py
+++
@@ -1,8 +0,0 @@
-class Animal():
-    def __init__(self,name,gender,age):
-        self.name = name
-        self.gender = gender
-        self.age = age
-
-    def __repr__(self):
-        return f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}"
