79fc01988156457e8d6e2f5083fbec7e28a468b1
D-on3 <104354779+D-on3@users.noreply.github.com>
2023-02-03 23:41:40 +0200

    organize dirs

---
+++ Python-OOP-June-2022/00_EXAM_PREP/exam_Aug-23-2021/structure_func/project/planet/planet.py
@@ -0,0 +1,18 @@
+class Planet:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value.strip() == '':
+            raise ValueError("Planet name cannot be empty string or whitespace!")
+        self.__name = value
+
+
+
+
