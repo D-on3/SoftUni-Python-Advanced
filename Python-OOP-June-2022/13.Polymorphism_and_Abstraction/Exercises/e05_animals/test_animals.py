c5f815a08eec23866738f3bbb5af8ac015228053
D-on3 <104354779+D-on3@users.noreply.github.com>
2023-02-03 23:42:54 +0200

    organize dirs

---
+++ Python-OOP-June-2022/13.Polymorphism_and_Abstraction/Exercises/e05_animals/test_animals.py
@@ -0,0 +1,18 @@
+from cat import Cat
+from dog import Dog
+from kitten import Kitten
+from tomcat import Tomcat
+
+dog = Dog("Rocky", 3, "Male")
+print(dog.make_sound())
+print(dog)
+tomcat = Tomcat("Tom", 6)
+print(tomcat.make_sound())
+print(tomcat)
+
+kitten = Kitten("Kiki", 1)
+print(kitten.make_sound())
+print(kitten)
+cat = Cat("Johnny", 7, "Male")
+print(cat.make_sound())
+print(cat)
